c6ac14480cb0ade856c69c3c5c7d9f3312555adb
@Jovanzers <jovanzers@gmail.com>
2022-05-27 21:30:24 +0700

    Update

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -5,7 +5,7 @@
 
 /* global document, Office, Word */
 
-Office.onReady(info => {
+Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
@@ -14,7 +14,7 @@ Office.onReady(info => {
 });
 
 export async function run() {
-  return Word.run(async context => {
+  return Word.run(async (context) => {
     /**
      * Insert your Word code here
      */
